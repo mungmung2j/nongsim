ae1ac782e2eb2008888f26af4b1edff70feb1a2f
SJM <warning6715@gmail.com>
2023-05-18 23:16:28 +0900

    캐릭터 마우스오버 이미지변경, 상품 마우스오버 테두리, 스와이퍼 기능 추가

--- nongsim.js
+++ nongsim.js
@@ -7,3 +7,59 @@ character.addEventListener('mouseover', function(){
 character.addEventListener('mouseleave', function(){
   character.classList.remove('over')
 })
+
+const border=document.querySelectorAll('.production')
+// ['.production1', '.production2']
+//const a = [1,2,3,4]
+
+for (let i = 0; i < border.length; i++) {
+  border[i].addEventListener('mouseover', function(){
+    border[i].classList.add('over')
+  })
+}
+
+for (let i=0; i<border.length; i++){
+  border[i].addEventListener('mouseleave', function(){
+    border[i].classList.remove('over')
+  })
+}
+
+
+// var mySwiper=new Swiper('.swiper-container',{
+//   direction: 'vertical',
+//   loop: true,
+
+//   navigation:{
+//     nextEl:'.swiper-button-next',
+//     prevEl:'.swiper-button-prev',
+//   }
+// })
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 4,  
+  slidesPerGroup: 1,
+  spaceBetween: 20,
+  autoplay: {     //자동슬라이드 (false-비활성화)
+    delay: 2500, // 시간 설정
+    disableOnInteraction: true, // false-스와이프 후 자동 재생
+  },
+  loopAdditionalSlides: 1,
+
+  // If we need pagination
+  pagination: {
+    
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    
+  },
+});
